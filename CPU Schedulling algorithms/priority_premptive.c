46fc3606ddef0da2d82e689e26c43ff34fe03a68
Abhishek S <abhisheks3002@gmai.com>
2023-08-08 21:25:44 +0530

    renamed

---
+++ CPU Schedulling algorithms/priority_premptive.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+struct process
+{
+    int pid;
+    int at;
+    int bt;
+    int rt;
+    int wt;
+    int tat;
+    int p;
+} p[10], temp;
+
+void main()
+{
+    int n, i, j, ct = 0, comp = 0, prev = -1, curr = -1;
+    float avg_wt = 0, avg_tat = 0;
+
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+        printf("Enter the arrival time ,burst time and priority of process %d: ", i);
+        scanf("%d %d %d", &p[i].at, &p[i].bt, &p[i].p);
+        p[i].pid = i;
+        p[i].rt = p[i].bt;
+    }
+
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (p[j].at > p[j + 1].at)
+            {
+                temp = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("\nGantt Chart:\n");
+    while (comp < n)
+    {
+        curr = -1;
+        for (i = 0; i < n; i++)
+        {
+            if (p[i].at <= ct && p[i].rt > 0 && (curr == -1 || p[i].p < p[curr].p))
+                curr = i;
+        }
+        if (curr != -1)
+        {
+            if (prev != curr)
+                printf("%d | P%d | ", ct, p[curr].pid);
+            ct++;
+            p[curr].rt--;
+            if (p[curr].rt == 0)
+            {
+                p[curr].tat = ct - p[curr].at;
+                p[curr].wt = p[curr].tat - p[curr].bt;
+                comp++;
+            }
+            prev = curr;
+        }
+        else
+        {
+            printf("%d | IDLE | ", ct);
+            ct++;
+        }
+    }
+    printf("%d", ct);
+
+    printf("\n+-----+--------------+------------+--------------+-----------------+\n");
+    printf("| PID | Arrival Time | Burst Time | Waiting Time | Turnaround Time |\n");
+    printf("+-----+--------------+------------+--------------+-----------------+\n");
+    for (int i = 0; i < n; i++)
+        printf("| %3d | %12d | %10d | %12d | %15d |\n", p[i].pid, p[i].at, p[i].bt, p[i].wt, p[i].tat);
+    printf("+-----+--------------+------------+--------------+-----------------+\n");
+
+    for (int i = 0; i < n; i++)
+    {
+        avg_wt += p[i].wt;
+        avg_tat += p[i].tat;
+    }
+
+    avg_wt /= n;
+    avg_tat /= n;
+    printf("Average waiting time: %.2f\n", avg_wt);
+    printf("Average turnaround time: %.2f\n", avg_tat);
+}
